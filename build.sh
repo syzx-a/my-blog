09254976ffe39a766cce47e1bf2369a25e82030e
user <user@example.com>
2026-02-19 16:15:18 +0800

    Update Hugo version to 0.146.4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -sL https://github.com/gohugoio/hugo/releases/download/v0.123.0/hugo_extended_0.123.0_linux-amd64.tar.gz | tar -xz
+curl -sL https://github.com/gohugoio/hugo/releases/download/v0.146.4/hugo_extended_0.146.4_linux-amd64.tar.gz | tar -xz
 ./hugo --gc --minify
